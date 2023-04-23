7748368641ab116bec234d8cde4b04d9f941e1b6
thedavidmeister <thedavidmeister@gmail.com>
2023-04-23 21:56:50 +0400

    wip on compiling

--- src/LibOp.sol
+++ src/LibOp.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
+import "rain.interface.interpreter/IInterpreterV1.sol";
+import "sol.lib.memory/LibPointer.sol";
+import "sol.lib.memory/LibMemCpy.sol";
+import "sol.lib.memory/LibStackPointer.sol";
+
+/// Thrown when the length of an array as the result of an applied function does
+/// not match expectations.
+error UnexpectedResultLength(uint256 expectedLength, uint256 actualLength);
+
 library LibOp {
+    using LibStackPointer for Pointer;
+
     /// Execute a function, reading and writing inputs and outputs on the stack.
     /// The caller MUST ensure this does not result in unsafe reads and writes.
     /// @param pointer The stack top to read and write to.
@@ -53,10 +64,10 @@ library LibOp {
     /// @param stackTop_ The stack top to read and write to.
     /// @param fn_ The function to run on the stack.
     /// @return The new stack top above the outputs of fn_.
-    function applyFn(StackPointer stackTop_, function(uint256, uint256) internal view returns (uint256) fn_)
+    function applyFn(Pointer stackTop_, function(uint256, uint256) internal view returns (uint256) fn_)
         internal
         view
-        returns (StackPointer)
+        returns (Pointer)
     {
         uint256 a_;
         uint256 b_;
@@ -81,24 +92,24 @@ library LibOp {
     /// @param fn_ The function to run on the stack.
     /// @param n_ The number of times to apply fn_ to accumulate a final result.
     /// @return stackTopAfter_ The new stack top above the outputs of fn_.
-    function applyFnN(
-        StackPointer stackTop_,
-        function(uint256, uint256) internal view returns (uint256) fn_,
-        uint256 n_
-    ) internal view returns (StackPointer) {
+    function applyFnN(Pointer stackTop_, function(uint256, uint256) internal view returns (uint256) fn_, uint256 n_)
+        internal
+        view
+        returns (Pointer)
+    {
         unchecked {
             uint256 bottom_;
             uint256 cursor_;
             uint256 a_;
             uint256 b_;
-            StackPointer stackTopAfter_;
+            Pointer stackTopAfter_;
             assembly ("memory-safe") {
                 bottom_ := sub(stackTop_, mul(n_, 0x20))
                 a_ := mload(bottom_)
                 stackTopAfter_ := add(bottom_, 0x20)
                 cursor_ := stackTopAfter_
             }
-            while (cursor_ < StackPointer.unwrap(stackTop_)) {
+            while (cursor_ < Pointer.unwrap(stackTop_)) {
                 assembly ("memory-safe") {
                     b_ := mload(cursor_)
                 }
@@ -119,19 +130,19 @@ library LibOp {
     /// @param fn_ The function to run on the stack.
     /// @param n_ The number of times to apply fn_ to accumulate a final result.
     /// @return stackTopAfter_ The new stack top above the outputs of fn_.
-    function applyFnN(StackPointer stackTop_, function(uint256) internal view fn_, uint256 n_)
+    function applyFnN(Pointer stackTop_, function(uint256) internal view fn_, uint256 n_)
         internal
         view
-        returns (StackPointer)
+        returns (Pointer)
     {
         uint256 cursor_;
         uint256 a_;
-        StackPointer stackTopAfter_;
+        Pointer stackTopAfter_;
         assembly ("memory-safe") {
             stackTopAfter_ := sub(stackTop_, mul(n_, 0x20))
             cursor_ := stackTopAfter_
         }
-        while (cursor_ < StackPointer.unwrap(stackTop_)) {
+        while (cursor_ < Pointer.unwrap(stackTop_)) {
             assembly ("memory-safe") {
                 a_ := mload(cursor_)
                 cursor_ := add(cursor_, 0x20)
@@ -146,10 +157,10 @@ library LibOp {
     /// @param stackTop_ The stack top to read and write to.
     /// @param fn_ The function to run on the stack.
     /// @return The new stack top above the outputs of fn_.
-    function applyFn(StackPointer stackTop_, function(uint256, uint256, uint256) internal view returns (uint256) fn_)
+    function applyFn(Pointer stackTop_, function(uint256, uint256, uint256) internal view returns (uint256) fn_)
         internal
         view
-        returns (StackPointer)
+        returns (Pointer)
     {
         uint256 a_;
         uint256 b_;
@@ -175,12 +186,12 @@ library LibOp {
     /// @param fn_ The function to run on the stack.
     /// @return The new stack top above the outputs of fn_.
     function applyFn(
-        StackPointer stackTop_,
+        Pointer stackTop_,
         function(uint256, uint256, uint256, uint256)
             internal
             view
             returns (uint256) fn_
-    ) internal view returns (StackPointer) {
+    ) internal view returns (Pointer) {
         uint256 a_;
         uint256 b_;
         uint256 c_;
@@ -208,10 +219,10 @@ library LibOp {
     /// @param operand_ Operand is passed from the source instead of the stack.
     /// @return The new stack top above the outputs of fn_.
     function applyFn(
-        StackPointer stackTop_,
+        Pointer stackTop_,
         function(Operand, uint256, uint256) internal view returns (uint256) fn_,
         Operand operand_
-    ) internal view returns (StackPointer) {
+    ) internal view returns (Pointer) {
         uint256 a_;
         uint256 b_;
         uint256 location_;
@@ -230,18 +241,18 @@ library LibOp {
 
     /// Execute a function, reading and writing inputs and outputs on the stack.
     /// The caller MUST ensure this does not result in unsafe reads and writes.
-    /// @param stackTop_ The stack top to read and write to.
-    /// @param fn_ The function to run on the stack.
-    /// @param length_ The length of the array to pass to fn_ from the stack.
-    /// @return stackTopAfter_ The new stack top above the outputs of fn_.
-    function applyFn(
-        StackPointer stackTop_,
-        function(uint256[] memory) internal view returns (uint256) fn_,
-        uint256 length_
-    ) internal view returns (StackPointer) {
-        (uint256 a_, uint256[] memory tail_) = stackTop_.list(length_);
-        uint256 b_ = fn_(tail_);
-        return tail_.asStackPointer().push(a_).push(b_);
+    /// @param stackTop The stack top to read and write to.
+    /// @param f The function to run on the stack.
+    /// @param length The length of the array to pass to f from the stack.
+    /// @return The new stack top above the outputs of f.
+    function applyFn(Pointer stackTop, function(uint256[] memory) internal view returns (uint256) f, uint256 length)
+        internal
+        view
+        returns (Pointer)
+    {
+        (uint256 a, uint256[] memory tail) = stackTop.unsafeList(length);
+        uint256 b = f(tail);
+        return tail.asStackPointer().push(a).push(b);
     }
 
     /// Execute a function, reading and writing inputs and outputs on the stack.
@@ -251,16 +262,16 @@ library LibOp {
     /// @param length_ The length of the array to pass to fn_ from the stack.
     /// @return stackTopAfter_ The new stack top above the outputs of fn_.
     function applyFn(
-        StackPointer stackTop_,
+        Pointer stackTop_,
         function(uint256, uint256, uint256[] memory)
             internal
             view
             returns (uint256) fn_,
         uint256 length_
-    ) internal view returns (StackPointer) {
+    ) internal view returns (Pointer) {
         (uint256 b_, uint256[] memory tail_) = stackTop_.list(length_);
-        StackPointer stackTopAfter_ = tail_.asStackPointer();
-        (StackPointer location_, uint256 a_) = stackTopAfter_.pop();
+        Pointer stackTopAfter_ = tail_.asStackPointer();
+        (Pointer location_, uint256 a_) = stackTopAfter_.pop();
         location_.set(fn_(a_, b_, tail_));
         return stackTopAfter_;
     }
@@ -272,15 +283,15 @@ library LibOp {
     /// @param length_ The length of the array to pass to fn_ from the stack.
     /// @return The new stack top above the outputs of fn_.
     function applyFn(
-        StackPointer stackTop_,
+        Pointer stackTop_,
         function(uint256, uint256, uint256, uint256[] memory)
             internal
             view
             returns (uint256) fn_,
         uint256 length_
-    ) internal view returns (StackPointer) {
+    ) internal view returns (Pointer) {
         (uint256 c_, uint256[] memory tail_) = stackTop_.list(length_);
-        (StackPointer stackTopAfter_, uint256 b_) = tail_.asStackPointer().pop();
+        (Pointer stackTopAfter_, uint256 b_) = tail_.asStackPointer().pop();
         uint256 a_ = stackTopAfter_.peek();
         stackTopAfter_.down().set(fn_(a_, b_, c_, tail_));
         return stackTopAfter_;
@@ -293,16 +304,16 @@ library LibOp {
     /// @param length_ The length of the arrays to pass to fn_ from the stack.
     /// @return The new stack top above the outputs of fn_.
     function applyFn(
-        StackPointer stackTop_,
+        Pointer stackTop_,
         function(uint256, uint256[] memory, uint256[] memory)
             internal
             view
             returns (uint256[] memory) fn_,
         uint256 length_
-    ) internal view returns (StackPointer) {
-        StackPointer csStart_ = stackTop_.down(length_);
+    ) internal view returns (Pointer) {
+        Pointer csStart_ = stackTop_.down(length_);
         uint256[] memory cs_ = new uint256[](length_);
-        LibMemCpy.unsafeCopyWordsTo(Pointer.wrap(StackPointer.unwrap(csStart_)), cs_.dataPointer(), length_);
+        LibMemCpy.unsafeCopyWordsTo(csStart_, cs_.dataPointer(), length_);
         (uint256 a_, uint256[] memory bs_) = csStart_.list(length_);
 
         uint256[] memory results_ = fn_(a_, bs_, cs_);
@@ -310,8 +321,8 @@ library LibOp {
             revert UnexpectedResultLength(length_, results_.length);
         }
 
-        StackPointer bottom_ = bs_.asStackPointer();
-        LibMemCpy.unsafeCopyWordsTo(results_.dataPointer(), Pointer.wrap(StackPointer.unwrap(bottom_)), length_);
+        Pointer bottom_ = bs_.asStackPointer();
+        LibMemCpy.unsafeCopyWordsTo(results_.dataPointer(), bottom_, length_);
         return bottom_.up(length_);
     }
 }
