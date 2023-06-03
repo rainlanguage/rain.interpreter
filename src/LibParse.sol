688939c03c9a27cf8f10f0604a6434a51aee688d
thedavidmeister <thedavidmeister@gmail.com>
2023-06-03 21:45:22 +0400

    empties

--- src/LibParse.sol
+++ src/LibParse.sol
@@ -1,49 +1,78 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
+error MissingFinalSemi();
+
 library LibParse {
     function parse(bytes memory data) internal pure returns (bytes[] memory sources, uint256[] memory constants) {
-        assembly ("memory-safe") {
-            // Notable excerpts from ASCII as shifted chars
-            // 0x20 = space = 0x0100000000
-            // 0x2C = , = 0x100000000000
-            // 0x3A = : = 0x0400000000000000
-            // 0x3B = ; = 0x0800000000000000
-            let lhs := 1
-            let cursor := add(data, 1)
-            let end := add(cursor, mload(data))
-            // This is spacebar in ascii, effectively this prefixes every
-            // parsed data with some whitespace.
-            let prevChar := 0x0100000000
-            let char := shl(and(mload(cursor), 0xFF), 1)
-            for {} lt(cursor, end) {} {
-                prevChar := char
-                // Cursor must be incremented by the inner logic.
-                char := shl(and(mload(cursor), 0xFF), 1)
-                let masked := and(
-                    char,
-                    // mask for , : ;
-                    0x0C00100000000000
-                )
-                if masked {
-                    switch masked
-                    // ,
-                    case 0x100000000000 {
-                        lhs := 1
-                        cursor := add(cursor, 1)
+        if (data.length > 0) {
+            uint256 char;
+            assembly ("memory-safe") {
+                // Notable excerpts from ASCII as shifted chars
+                // 0x20 = space = 0x0100000000
+                // 0x2C = , = 0x100000000000
+                // 0x3A = : = 0x0400000000000000
+                // 0x3B = ; = 0x0800000000000000
+                let lhs := 1
+                let outputCursor := mload(0x40)
+
+                let source := outputCursor
+                mstore(source, 0)
+                outputCursor := add(outputCursor, 0x20)
+
+                let cursor := add(data, 1)
+                let end := add(cursor, mload(data))
+                for {} lt(cursor, end) { cursor := add(cursor, 1) } {
+                    // Cursor must be incremented by the inner logic.
+                    char := shl(and(mload(cursor), 0xFF), 1)
+                    let masked :=
+                        and(
+                            char,
+                            // mask for , : ;
+                            0x0C00100000000000
+                        )
+                    if masked {
+                        switch masked
+                        // ,
+                        // rhs end
+                        case 0x100000000000 { lhs := 1 }
+                        // char :
+                        // lhs end
+                        case 0x0400000000000000 { lhs := 0 }
+                        // char ;
+                        // source end
+                        // implies rhs end
+                        case 0x0800000000000000 {
+                            lhs := 1
+
+                            // Brute force a new list of references every time we
+                            // encounter a new source. We assume that most parsed
+                            // data will have a low number of sources, ~5 or less.
+                            let oldSourcesLength := mload(sources)
+                            let sourcesCursor := add(sources, 0x20)
+                            sources := outputCursor
+
+                            mstore(outputCursor, add(oldSourcesLength, 1))
+                            outputCursor := add(outputCursor, 0x20)
+
+                            let sourcesEnd := add(sourcesCursor, mul(oldSourcesLength, 0x20))
+                            for {} lt(sourcesCursor, sourcesEnd) {
+                                sourcesCursor := add(sourcesCursor, 0x20)
+                                outputCursor := add(outputCursor, 0x20)
+                            } { mstore(outputCursor, mload(sourcesCursor)) }
+                            mstore(outputCursor, source)
+                            outputCursor := add(outputCursor, 0x20)
+                            source := outputCursor
+                        }
+                        // unreachable, implies broken mask.
+                        default { revert(0, 0) }
+                        continue
                     }
-                    // char :
-                    case 0x0400000000000000 {
-                        lhs := 0
-                        cursor := add(cursor, 1)
-                    }
-                    // char ;
-                    case 0x0800000000000000 { cursor := add(cursor, 1) }
-                    default { revert(0, 0) }
                 }
+                mstore(0x40, outputCursor)
             }
-            if iszero(eq(char, 0x0800000000000000)) {
-                revert(0, 0)
+            if (char != 0x0800000000000000) {
+                revert MissingFinalSemi();
             }
         }
     }
--- test/LibParse.t.sol
+++ test/LibParse.t.sol
@@ -6,13 +6,37 @@ import "forge-std/Test.sol";
 import "src/LibParse.sol";
 
 contract LibParseTest is Test {
-    function testParseEmpty() external pure {
-        // bytes memory char  = ";";
-        // assembly {
-        //     // let x := shl(and(mload(add(char, 1)), 0xFF), 1)
-        //     let x := or(or(0x100000000000, 0x0400000000000000), 0x0800000000000000)
-        //     let y := mload(x)
-        // }
-        (bytes[] memory sources, uint256[] memory constants) = LibParse.parse(":;");
+    function testCharBuilder() external pure {
+        bytes memory char = ";";
+        assembly {
+            let x := shl(and(mload(add(char, 1)), 0xFF), 1)
+            // let x := or(or(0x100000000000, 0x0400000000000000), 0x0800000000000000)
+            let y := mload(x)
+        }
+    }
+
+    function testParseEmpty() external {
+        (bytes[] memory sources0, uint256[] memory constants0) = LibParse.parse("");
+        assertEq(sources0.length, 0);
+        assertEq(constants0.length, 0);
+
+        (bytes[] memory sources1, uint256[] memory constants1) = LibParse.parse(":;");
+        assertEq(sources1.length, 1);
+        assertEq(constants1.length, 0);
+
+        (bytes[] memory sources2, uint256[] memory constants2) = LibParse.parse(":;:;");
+        assertEq(sources2.length, 2);
+        assertEq(constants2.length, 0);
+    }
+
+    function testParseMissingFinalSemi() external {
+        vm.expectRevert(abi.encodeWithSelector(MissingFinalSemi.selector));
+        LibParse.parse(":");
+
+        vm.expectRevert(abi.encodeWithSelector(MissingFinalSemi.selector));
+        LibParse.parse(":;:");
+
+        vm.expectRevert(abi.encodeWithSelector(MissingFinalSemi.selector));
+        LibParse.parse("::");
     }
 }
