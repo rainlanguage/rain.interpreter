5a4d91cdf0eb6c98fcd62072b9f463092efd32c0
thedavidmeister <thedavidmeister@gmail.com>
2023-08-22 18:21:02 +0400

    deploy dispair on ci

---
+++ script/DeployDISPair.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import "forge-std/Script.sol";
+import "src/concrete/RainterpreterStore.sol";
+import "src/concrete/RainterpreterNP.sol";
+
+/// @title DeployDISPair
+/// @notice A script that deploys a DeployDISPair.
+/// This is intended to be run on every commit by CI to a testnet such as mumbai,
+/// then cross chain deployed to whatever mainnet is required, by users.
+contract DeployDISPair is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        RainterpreterNP interpreter = new RainterpreterNP();
+        RainterpreterStore store = new RainterpreterStore();
+        RainterpreterExpressionDeployerNP deployer = new RainterpreterExpressionDeployerNP(RainterpreterExpressionDeployerConstructionConfig(
+            address(interpreter),
+            address(store),
+            LibAllStandardOpsNP.authoringMeta()
+        ));
+        vm.stopBroadcast();
+    }
+}
