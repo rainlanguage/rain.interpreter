8ec14ad2f6e36d3daa1c4e962cb5512348bca336
thedavidmeister <thedavidmeister@gmail.com>
2023-09-28 13:02:44 +0400

    docs for call

--- src/lib/op/call/LibOpCallNP.sol
+++ src/lib/op/call/LibOpCallNP.sol
@@ -14,6 +14,60 @@ import {LibEvalNP} from "../../eval/LibEvalNP.sol";
 /// @param outputs The number of outputs requested by the operand.
 error CallOutputsExceedSource(uint256 sourceOutputs, uint256 outputs);
 
+/// @title LibOpCallNP
+/// @notice Contains the call operation. This allows sources to be treated in a
+/// function-like manner. Primarily intended as a way for expression authors to
+/// create reusable logic inline with their expression, in a way that mimics how
+/// words and stack consumption works at the Solidity level.
+///
+/// Similarities between `call` and a traditional function:
+/// - The source is called with a set of 0+ inputs.
+/// - The source returns a set of 0+ outputs.
+/// - The source has a fixed number of inputs and outputs.
+/// - When the source executes it has its own stack/scope.
+/// - The source can be called from multiple places.
+/// - The source can `call` other sources.
+/// - The source is stateless across calls
+///   (although it can use words like get/set to read/write external state).
+/// - The caller and callee have to agree on the number of inputs
+///   (but not outputs, see below).
+/// - Generally speaking, the behaviour of a source can be reasoned about
+///   without needing to know the context in which it is called. Which is the
+///   basic requirement for reusability.
+///
+/// Differences between `call` and a traditional function:
+/// - The caller defines the number of outputs to be returned, NOT the callee.
+///   This is because the caller is responsible for allocating space on the
+///   stack for the outputs, and the callee is responsible for providing the
+///   outputs. The only limitation is that the caller cannot request more
+///   outputs than the callee has available. This means that two calls to the
+///   same source can return different numbers of outputs in different contexts.
+/// - The inputs to a source are considered to be the top of the callee's stack
+///   from the perspective of the caller. This means that the inputs are eligible
+///   to be read as outputs, if the caller chooses to do so.
+/// - The sources are not named, they are identified by their index in the
+///   bytecode. Tooling can provide sugar over this but the underlying
+///   representation is just an index.
+/// - Sources are not "first class" like functions often are, i.e. they cannot
+///   be passed as arguments to other sources or otherwise be treated as values.
+/// - Recursion is not supported. This is because currently there is no laziness
+///   in the interpreter, so a recursive call would result in an infinite loop
+///   unconditionally (even when wrapped in an `if`). This may change in the
+///   future.
+/// - The memory allocation for a source must be known at compile time.
+///
+/// The order of inputs and outputs is designed so that the visual representation
+/// of a source call matches the visual representation of a function call. This
+/// requires some reversals of order "under the hood" while copying data around
+/// but it makes the behaviour of `call` more intuitive.
+///
+/// Illustrative example:
+/// ```
+/// /* Final result */
+/// /* a = 2 */
+/// /* b = 9 */
+/// a b: call<1 2>(10 5); ten five:, a b: int-div(ten five) 9;
+/// ```
 library LibOpCallNP {
     using LibPointer for Pointer;
 
@@ -31,17 +85,22 @@ library LibOpCallNP {
         return (sourceInputs, outputs);
     }
 
+    /// The `call` word is conceptually very simple. It takes a source index, a
+    /// number of outputs, and a number of inputs. It then runs the standard
+    /// eval loop for the source, with a starting stack pointer above the inputs,
+    /// and then copies the outputs to the calling stack.
     function run(InterpreterStateNP memory state, Operand operand, Pointer stackTop) internal view returns (Pointer) {
+        // Extract config from the operand.
         uint256 sourceIndex = Operand.unwrap(operand) & 0xFF;
         uint256 outputs = (Operand.unwrap(operand) >> 8) & 0xFF;
         uint256 inputs = (Operand.unwrap(operand) >> 0x10) & 0xFF;
-        Pointer[] memory stackBottoms = state.stackBottoms;
-        Pointer evalStackTop;
 
         // Copy inputs in. The inputs have to be copied in reverse order so that
         // the top of the stack from the perspective of `call`, i.e. the first
         // input to call, is the bottom of the stack from the perspective of the
         // callee.
+        Pointer[] memory stackBottoms = state.stackBottoms;
+        Pointer evalStackTop;
         assembly ("memory-safe") {
             evalStackTop := mload(add(stackBottoms, mul(add(sourceIndex, 1), 0x20)))
             let end := add(stackTop, mul(inputs, 0x20))
@@ -51,9 +110,17 @@ library LibOpCallNP {
             }
         }
 
+        // Keep a copy of the current source index so that we can restore it
+        // after the call.
         uint256 currentSourceIndex = state.sourceIndex;
+
+        // Set the state to the source we are calling.
         state.sourceIndex = sourceIndex;
+
+        // Run the eval loop.
         evalStackTop = LibEvalNP.evalLoopNP(state, evalStackTop);
+
+        // Restore the source index in the state.
         state.sourceIndex = currentSourceIndex;
 
         // Copy outputs out.
--- test/lib/op/call/LibOpCallNP.t.sol
+++ test/lib/op/call/LibOpCallNP.t.sol
@@ -94,200 +94,135 @@ contract LibOpCallNPTest is OpTest, BytecodeTest {
         assertEq(calcOutputs, outputs, "outputs");
     }
 
-    /// Test that the eval of a call into a source that doesn't exist reverts
-    /// upon deploy.
-    function testOpCallNPRunSourceDoesNotExist() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse("a b: call<1 2>(10 5);");
+    /// Boilerplate for testing that a source does not exist.
+    function checkSourceDoesNotExist(bytes memory rainlang, uint256 sourceIndex) internal {
+        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(rainlang);
         uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 2;
-        vm.expectRevert(abi.encodeWithSelector(SourceIndexOutOfBounds.selector, bytecode, 1));
+        minOutputs[0] = 0;
+        vm.expectRevert(abi.encodeWithSelector(SourceIndexOutOfBounds.selector, bytecode, sourceIndex));
         (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
             iDeployer.deployExpression(bytecode, constants, minOutputs);
         (interpreterDeployer, storeDeployer, expression);
     }
 
-    /// Test the eval of some call that has no inputs or outputs so we can sanity
-    /// check the stacks.
+    /// Test that the eval of a call into a source that doesn't exist reverts
+    /// upon deploy.
+    function testOpCallNPRunSourceDoesNotExist() external {
+        // 0 inputs and outputs different source indexes.
+        checkSourceDoesNotExist(": call<1 0>();", 1);
+        checkSourceDoesNotExist(": call<2 0>();", 2);
+        // 1 input and 0 outputs different source indexes.
+        checkSourceDoesNotExist(": call<1 0>(1);", 1);
+        checkSourceDoesNotExist(": call<2 0>(1);", 2);
+        // 0 inputs and 1 output different source indexes.
+        checkSourceDoesNotExist("_: call<1 1>();", 1);
+        checkSourceDoesNotExist("_: call<2 1>();", 2);
+        // Several inputs and outputs different source indexes.
+        checkSourceDoesNotExist("a b: call<1 2>(10 5);", 1);
+        checkSourceDoesNotExist("a b: call<2 2>(10 5);", 2);
+        // Multiple sources.
+        checkSourceDoesNotExist(": call<2 0>();:;", 2);
+        checkSourceDoesNotExist(": call<3 0>();:;", 3);
+    }
+
+    /// Boilerplate for checking the stack and kvs of a call.
+    function checkCallNPRun(bytes memory rainlang, uint256[] memory stack, uint256[] memory kvs) internal {
+        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(rainlang);
+        // The second source is for internal calls only, it is not an entrypoint.
+        uint256[] memory minOutputs = new uint256[](1);
+        minOutputs[0] = 0;
+        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
+            iDeployer.deployExpression(bytecode, constants, minOutputs);
+        (uint256[] memory actualStack, uint256[] memory actualKVs) = interpreterDeployer.eval(
+            storeDeployer,
+            StateNamespace.wrap(0),
+            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), type(uint8).max),
+            new uint256[][](0)
+        );
+        assertEq(actualStack.length, stack.length, "stack length");
+        for (uint256 i = 0; i < stack.length; i++) {
+            assertEq(actualStack[i], stack[i], "stack[i]");
+        }
+        assertEq(actualKVs.length, kvs.length, "kvs length");
+        for (uint256 i = 0; i < kvs.length; i++) {
+            assertEq(actualKVs[i], kvs[i], "kvs[i]");
+        }
+    }
+
+    /// Test the eval of call to see various stacks.
     function testOpCallNPRunNoIO() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(":call<1 0>();:;");
+        // Check evals that result in no stack or kvs.
+        uint256[] memory stack = new uint256[](0);
+        uint256[] memory kvs = new uint256[](0);
+        // 0 IO, call noop.
+        checkCallNPRun(":call<1 0>();:;", stack, kvs);
+        // Single input and no outputs.
+        checkCallNPRun(":call<1 0>(10);ten:;", stack, kvs);
+
+        // Check evals that result in a stack of one item but no kvs.
+        stack = new uint256[](1);
+        // Single input and single output.
+        stack[0] = 10;
+        checkCallNPRun("ten:call<1 1>(10);ten:;", stack, kvs);
+        // zero input single output.
+        checkCallNPRun("ten:call<1 1>();ten:10;", stack, kvs);
+        // Two inputs and one output.
+        stack[0] = 12;
+        checkCallNPRun("a: call<1 1>(10 11); ten eleven:,a b c:ten eleven 12;", stack, kvs);
+
+        // Check evals that result in a stack of two items but no kvs.
+        stack = new uint256[](2);
+        // Order dependent inputs and outputs.
+        stack[0] = 2;
+        stack[1] = 9;
+        checkCallNPRun("a b: call<1 2>(10 5); ten five:, a b: int-div(ten five) 9;", stack, kvs);
+
+        // One input two outputs.
+        stack[0] = 10;
+        stack[1] = 11;
+        checkCallNPRun("a b: call<1 2>(10); ten:,a b:ten 11;", stack, kvs);
+
+        // Can call something with no IO purely for the kv side effects.
+        stack = new uint256[](0);
+        kvs = new uint256[](2);
+        kvs[0] = 10;
+        kvs[1] = 11;
+        checkCallNPRun(":call<1 0>();:set(10 11);", stack, kvs);
+
+        // Can call for side effects and also get a stack based on IO.
+        stack = new uint256[](1);
+        stack[0] = 10;
+        checkCallNPRun("a:call<1 1>(9);nine:,:set(10 11),ret:int-add(nine 1);", stack, kvs);
+    }
+
+    /// Boilerplate to check a generic runtime error happens upon recursion.
+    function checkCallNPRunRecursive(bytes memory rainlang) internal {
+        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(rainlang);
         // The second source is for internal calls only, it is not an entrypoint.
         uint256[] memory minOutputs = new uint256[](1);
         minOutputs[0] = 0;
+        // Recursion isn't caught at deploy time.
         (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
             iDeployer.deployExpression(bytecode, constants, minOutputs);
+        // But it will unconditionally happen at runtime.
+        vm.expectRevert();
         (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
             storeDeployer,
             StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
+            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), type(uint8).max),
             new uint256[][](0)
         );
-
-        assertEq(stack.length, 0, "stack length");
-        assertEq(kvs.length, 0, "kvs length");
-    }
-
-    /// Test the eval of some call with a single input and no outputs so we can
-    /// sanity check the stacks.
-    function testOpCallNPRunSingleInput() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(": call<1 0>(10); ten:;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 0;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 0, "stack length");
-        assertEq(kvs.length, 0, "kvs length");
-    }
-
-    /// Test the eval of some call with a single input and a single output so we
-    /// can sanity check the stacks.
-    function testOpCallNPRunSingleInputSingleOutput() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse("ten: call<1 1>(10); ten:;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 1;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 1, "stack length");
-        assertEq(stack[0], 10, "stack[0]");
-        assertEq(kvs.length, 0, "kvs length");
-    }
-
-    /// Test the eval of some call with a zero input and a single output so we
-    /// can sanity check the stacks.
-    function testOpCallNPRunZeroInputSingleOutput() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse("ten: call<1 1>(); ten:10;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 1;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 1, "stack length");
-        assertEq(stack[0], 10, "stack[0]");
-        assertEq(kvs.length, 0, "kvs length");
+        (stack, kvs);
     }
 
     /// Test that recursive calls are a (very gas intensive) runtime error.
     function testOpCallNPRunRecursive() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(":call<0 0>();");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 0;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        vm.expectRevert();
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-        (stack, kvs);
-    }
-
-    /// Test that transitive call loops are a (very gas intensive) runtime error.
-    function testOpCallNPRunCallLoop() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse(": call<1 0>();: call<0 0>();");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 0;
-        // vm.expectRevert(abi.encodeWithSelector(LibOpCallNP.run.selector, bytecode, constants, minOutputs, 0, 0, 0, 0, 0, 0));
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        vm.expectRevert();
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-        (stack, kvs);
-    }
-
-    /// Test the eval of some call that has order dependent inputs and outputs
-    /// so we can sanity check the stacks.
-    function testOpCallNPRunOrdering() external {
-        (bytes memory bytecode, uint256[] memory constants) =
-            iDeployer.parse("a b: call<1 2>(10 5); ten five:, a b: int-div(ten five) 9;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 2;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 2, "stack length");
-        assertEq(stack[0], 2, "stack[0]");
-        assertEq(stack[1], 9, "stack[1]");
-        assertEq(kvs.length, 0, "kvs length");
-    }
-
-    /// Test the eval with one input and two outputs.
-    function testOpCallNPRunSingleInputDoubleOutput() external {
-        (bytes memory bytecode, uint256[] memory constants) = iDeployer.parse("a b: call<1 2>(10); ten:,a b:ten 11;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 2;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 2, "stack length");
-        assertEq(stack[0], 10, "stack[0]");
-        assertEq(stack[1], 11, "stack[1]");
-        assertEq(kvs.length, 0, "kvs length");
-    }
-
-    /// Test the eval with two inputs and one output.
-    function testOpCallNPRunDoubleInputSingleOutput() external {
-        (bytes memory bytecode, uint256[] memory constants) =
-            iDeployer.parse("a: call<1 1>(10 11); ten eleven:,a b c:ten eleven 12;");
-        // The second source is for internal calls only, it is not an entrypoint.
-        uint256[] memory minOutputs = new uint256[](1);
-        minOutputs[0] = 1;
-        (IInterpreterV1 interpreterDeployer, IInterpreterStoreV1 storeDeployer, address expression) =
-            iDeployer.deployExpression(bytecode, constants, minOutputs);
-        (uint256[] memory stack, uint256[] memory kvs) = interpreterDeployer.eval(
-            storeDeployer,
-            StateNamespace.wrap(0),
-            LibEncodedDispatch.encode(expression, SourceIndex.wrap(0), 2),
-            new uint256[][](0)
-        );
-
-        assertEq(stack.length, 1, "stack length");
-        assertEq(stack[0], 12, "stack[0]");
-        assertEq(kvs.length, 0, "kvs length");
+        // Simple call self.
+        checkCallNPRunRecursive(":call<0 0>();");
+        // Ping pong between two calls.
+        checkCallNPRunRecursive(":call<1 0>();:call<0 0>();");
+        // If is eager so doesn't help.
+        checkCallNPRunRecursive("a:call<1 1>(1);do-call:,a:if(do-call call<1 1>(0) 5);");
     }
 
     /// Test a mismatch in the inputs from caller and callee.
