00a64e829fdb070dc178ba9bedec9c132b7f91b9
thedavidmeister <thedavidmeister@gmail.com>
2023-09-29 11:23:27 +0400

    ctpop word

--- src/lib/op/bitwise/LibOpCtPopNP.sol
+++ src/lib/op/bitwise/LibOpCtPopNP.sol
@@ -5,29 +5,33 @@ import {Pointer} from "rain.solmem/lib/LibPointer.sol";
 import {Operand} from "src/interface/IInterpreterV1.sol";
 import {InterpreterStateNP} from "../../state/LibInterpreterStateNP.sol";
 import {IntegrityCheckStateNP} from "../../integrity/LibIntegrityCheckNP.sol";
+import {LibCtPop} from "src/lib/bitwise/LibCtPop.sol";
+import {LibCtPopSlow} from "test/lib/bitwise/LibCtPopSlow.sol";
 
-/// @title LibOpBlockNumberNP
-/// Implementation of the EVM `BLOCKNUMBER` opcode as a standard Rainlang opcode.
-library LibOpBlockNumberNP {
+library LibOpCtPopNP {
     function integrity(IntegrityCheckStateNP memory, Operand) internal pure returns (uint256, uint256) {
-        return (0, 1);
+        return (1, 1);
     }
 
-    function run(InterpreterStateNP memory, Operand, Pointer stackTop) internal view returns (Pointer) {
+    function run(InterpreterStateNP memory, Operand, Pointer stackTop) internal pure returns (Pointer) {
+        uint256 value;
         assembly ("memory-safe") {
-            stackTop := sub(stackTop, 0x20)
-            mstore(stackTop, number())
+            value := mload(stackTop)
+        }
+        value = LibCtPop.ctpop(value);
+        assembly ("memory-safe") {
+            mstore(stackTop, value)
         }
         return stackTop;
     }
 
-    function referenceFn(InterpreterStateNP memory, Operand, uint256[] memory)
+    function referenceFn(InterpreterStateNP memory, Operand, uint256[] memory inputs)
         internal
-        view
+        pure
         returns (uint256[] memory)
     {
         uint256[] memory outputs = new uint256[](1);
-        outputs[0] = block.number;
+        outputs[0] = LibCtPopSlow.ctpopSlow(inputs[0]);
         return outputs;
     }
 }
