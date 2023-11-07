5222a43e9a088b41a7eca4a3d283721fccbc1058
thedavidmeister <thedavidmeister@gmail.com>
2023-11-07 12:13:41 +0400

    lock nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     rain.url = "github:rainprotocol/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
   };
