e6972f1740db27340b294ea5eca38c197be1d4a2
NanezX <victor.nanezj@gmail.com>
2023-11-17 18:18:30 -0400

    added submodule

---
+++ subgraph/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Flake for development interpreter subgraph workflows.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in rec {
+        packages = rec {
+          init-setup =  pkgs.writeShellScriptBin "init-setup" (''
+            git submodule update --init --recursive
+          '');
+
+          default = init-setup;
+        };
+      }
+    );
+}
