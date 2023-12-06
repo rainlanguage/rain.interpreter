3326927468c2ce25a4f60dab53fa277c263bef18
thedavidmeister <thedavidmeister@gmail.com>
2023-12-06 14:02:30 +0400

    meta hash

--- src/concrete/RainterpreterExpressionDeployerNPE2.sol
+++ src/concrete/RainterpreterExpressionDeployerNPE2.sol
@@ -36,7 +36,7 @@ bytes constant INTEGRITY_FUNCTION_POINTERS =
     hex"0a650adf0b480cc00cc00cca0cd30cee0d940d940df00e6c0e760e800cc00cca0cc00cc00cca0e6c0e6c0e6c0e6c0e6c0e8a0eac0ed60cc00e8a0cc00cc00e800cca0cc00cc00ef80ef80cc00cc00cca0cca0ef80ef80ef80ef80ef80ef80ef80ef80ef80ef80ef80ef80cca0f0f0f190f190f19";
 
 /// @dev Hash of the known construction meta.
-bytes32 constant CONSTRUCTION_META_HASH = bytes32(0xde6e8a19e22ced071d1b2ed8cd81af11eaa812a8e1a2802e7a6020034f615384);
+bytes32 constant CONSTRUCTION_META_HASH = bytes32(0xb54a52906cc014539c68c995f9261d5f47de3437308ee7e93a36650dd191ecb6);
 
 /// All config required to construct a `RainterpreterNPE2`.
 /// @param interpreter The `IInterpreterV2` to use for evaluation. MUST match
