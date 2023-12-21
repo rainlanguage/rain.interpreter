d1b397e5149a9ace0ec8bf44a466b2458b2ab381
thedavidmeister <thedavidmeister@gmail.com>
2023-12-21 18:03:10 +0400

    test

--- test/concrete/RainterpreterReferenceExternNPE2.intInc.t.sol
+++ test/concrete/RainterpreterReferenceExternNPE2.intInc.t.sol
@@ -5,7 +5,8 @@ import {OpTest} from "test/util/abstract/OpTest.sol";
 import {
     RainterpreterReferenceExternNPE2,
     OPCODE_FUNCTION_POINTERS,
-    INTEGRITY_FUNCTION_POINTERS
+    INTEGRITY_FUNCTION_POINTERS,
+    OP_INDEX_INCREMENT
 } from "src/concrete/RainterpreterReferenceExternNPE2.sol";
 import {
     ExternDispatch,
@@ -15,6 +16,8 @@ import {
 import {Operand} from "src/interface/unstable/IInterpreterV2.sol";
 import {LibExtern} from "src/lib/extern/LibExtern.sol";
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
+import {COMPATIBLITY_V0} from "src/interface/unstable/ISubParserV1.sol";
+import {OPCODE_EXTERN} from "src/interface/unstable/IInterpreterV2.sol";
 
 contract RainterpreterReferenceExternNPE2IntIncTest is OpTest {
     using Strings for address;
@@ -63,5 +66,39 @@ contract RainterpreterReferenceExternNPE2IntIncTest is OpTest {
             expectedStack,
             "sugared inc 2 3 = 3 4"
         );
+    }
+
+    /// Directly test the subparsing of the reference extern opcode.
+    function testRainterpreterReferenceExternNPE2IntIncSubParse(uint16 constantsHeight, bytes1 ioByte) external {
+        // Extern "only" supports up to constant height of 0xFF.
+        constantsHeight = uint16(bound(constantsHeight, 0, 0xFF));
+        RainterpreterReferenceExternNPE2 subParser = new RainterpreterReferenceExternNPE2();
+
+        (bool success, bytes memory bytecode, uint256[] memory constants) = subParser.subParse(
+            COMPATIBLITY_V0, bytes.concat(bytes2(constantsHeight), ioByte, bytes("reference-extern-inc"))
+        );
+        assertTrue(success);
+
+        assertEq(bytecode.length, 4);
+        assertEq(uint256(uint8(bytecode[0])), OPCODE_EXTERN);
+        assertEq(bytecode[1], ioByte);
+        // High byte for extern opcode is the outputs which is the same as
+        // the inputs for inc.
+        assertEq(bytecode[2], ioByte);
+        // Low byte for the extern opcode is the constants index of the extern
+        // dispatch.
+        assertEq(uint16(uint8(bytecode[3])), constantsHeight);
+
+        assertEq(constants.length, 1);
+        (IInterpreterExternV3 decodedExtern, ExternDispatch decodedExternDispatch) =
+            LibExtern.decodeExternCall(EncodedExternDispatch.wrap(constants[0]));
+
+        // The sub parser is also the extern contract because the reference
+        // implementation includes both.
+        assertEq(address(decodedExtern), address(subParser));
+
+        (uint256 opcode, Operand operand) = LibExtern.decodeExternDispatch(decodedExternDispatch);
+        assertEq(opcode, OP_INDEX_INCREMENT);
+        assertEq(Operand.unwrap(operand), 0);
     }
 }
