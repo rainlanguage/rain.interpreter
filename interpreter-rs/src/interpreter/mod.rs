7988f3e7ee1d9c7dba98ff2ba72faf37ef910156
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-12-30 13:02:42 +0530

    tests

--- interpreter-rs/src/interpreter/mod.rs
+++ interpreter-rs/src/interpreter/mod.rs
@@ -31,3 +31,44 @@ impl IExpressionDeployerV3<Provider<Http>> {
     }
 }
 
+#[cfg(test)]
+pub mod test {
+    use ethers::contract::ContractError;
+    use ethers::providers::{Http, Provider};
+    use crate::registry::IExpressionDeployerV3;
+    use std::str::FromStr;
+    use std::sync::Arc;
+    use ethers::types::{H160, Address};
+
+    #[tokio::test]
+    pub async fn test_dispair_happy_path() -> Result<(),ContractError<Provider<Http>>>  {
+        let rpc_url = "https://polygon.llamarpc.com/".to_string();
+        let provider = Provider::<Http>::try_from(rpc_url.clone()).unwrap();
+        let deployer_npe2_address = H160::from_str("0xD61d03501E95D4B507566fB42Ca2299595c4B1e6").unwrap();
+        let deployer_npe2: IExpressionDeployerV3<ethers::providers::Provider<ethers::providers::Http>> =
+        IExpressionDeployerV3::new(deployer_npe2_address, Arc::new(provider.clone()));  
+
+        let disp = deployer_npe2.try_build_dispair().await?; 
+
+        assert_eq!(H160::from_str("0xD61d03501E95D4B507566fB42Ca2299595c4B1e6").unwrap(),disp.deployer.address());
+        assert_eq!(H160::from_str("0xf374faFE473D76bf9518e437B484FbdD5674daFf").unwrap(),disp.interpreter.address());
+        assert_eq!(H160::from_str("0x5b777FAca336c648262e9c65a3c7A372a08c205b").unwrap(),disp.store.address());
+        assert_eq!(H160::from_str("0x63954a113cbDB20A4fD1Ac7DD76c9eDA29727f2D").unwrap(),disp.parser.address());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    pub async fn test_dispair_fail() -> Result<(),ContractError<Provider<Http>>>  {
+        let rpc_url = "https://polygon.llamarpc.com/".to_string();
+        let provider = Provider::<Http>::try_from(rpc_url.clone()).unwrap();
+        let deployer_npe2_address = Address::zero();
+        let deployer_npe2: IExpressionDeployerV3<ethers::providers::Provider<ethers::providers::Http>> =
+        IExpressionDeployerV3::new(deployer_npe2_address, Arc::new(provider.clone()));  
+
+        let disp = deployer_npe2.try_build_dispair().await; 
+        assert!(disp.is_err());
+    
+        Ok(())
+    }
+}
