1b4ef8767adae7b6079231794649b5d5537f98ce
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-01-04 19:09:13 +0530

    fmt

--- interpreter-rs/src/registry.rs
+++ interpreter-rs/src/registry.rs
@@ -1,7 +1,13 @@
 use ethers::contract::abigen;
 
-abigen!(IInterpreterV2, "../out/IInterpreterV2.sol/IInterpreterV2.json");
-abigen!(IInterpreterStoreV1, "../out/IInterpreterStoreV1.sol/IInterpreterStoreV1.json");
+abigen!(
+    IInterpreterV2,
+    "../out/IInterpreterV2.sol/IInterpreterV2.json"
+);
+abigen!(
+    IInterpreterStoreV1,
+    "../out/IInterpreterStoreV1.sol/IInterpreterStoreV1.json"
+);
 abigen!(IParserV1, "../out/IParserV1.sol/IParserV1.json");
 abigen!(
     IExpressionDeployerV3,
