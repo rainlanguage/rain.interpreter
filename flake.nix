fb6b78fd9ea86aa12d2e37fee7aa9b75aaaa0b30
thedavidmeister <thedavidmeister@gmail.com>
2024-01-05 19:06:27 +0400

    update flake

--- flake.nix
+++ flake.nix
@@ -17,28 +17,29 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rain-cli = "${rain.defaultPackage.${system}}/bin/rain";
+        rain-cli-bin = "${rain.defaultPackage.${system}}/bin/rain";
+        forge-bin = "${foundry.defaultPackage.${system}}/bin/forge";
 
       in with pkgs; rec {
         packages = rec {
 
           build-dispair-meta-cmd = ''
-            ${rain-cli} meta build \
-              -i <(${rain-cli} meta solc artifact -c abi -i out/RainterpreterExpressionDeployerNPE2.sol/RainterpreterExpressionDeployerNPE2.json) -m solidity-abi-v2 -t json -e deflate -l en \
-              -i <(forge script --silent ./script/GetAuthoringMeta.sol && cat ./meta/AuthoringMeta.rain.meta) -m authoring-meta-v1 -t cbor -e deflate -l none \
+            ${rain-cli-bin} meta build \
+              -i <(${rain-cli-bin} meta solc artifact -c abi -i out/RainterpreterExpressionDeployerNPE2.sol/RainterpreterExpressionDeployerNPE2.json) -m solidity-abi-v2 -t json -e deflate -l en \
+              -i <(${forge-bin} script --silent ./script/GetAuthoringMeta.sol && cat ./meta/AuthoringMeta.rain.meta) -m authoring-meta-v1 -t cbor -e deflate -l none \
           '';
 
           build-meta = pkgs.writeShellScriptBin "build-meta" ''
             mkdir -p meta;
-            forge build --force;
+            ${forge-bin} build --force;
             ${(build-dispair-meta-cmd)} -o meta/RainterpreterExpressionDeployerNPE2.rain.meta;
           '';
 
           deploy-dispair = pkgs.writeShellScriptBin "deploy-dispair" (''
             set -euo pipefail;
             mkdir -p meta;
-            forge build --force;
-            forge script -vvvvv script/DeployDISPair.sol --legacy --verify --broadcast --rpc-url "''${CI_DEPLOY_RPC_URL}" --etherscan-api-key "''${EXPLORER_VERIFICATION_KEY}" \
+            ${forge-bin} build --force;
+            ${forge-bin} script -vvvvv script/DeployDISPair.sol --legacy --verify --broadcast --rpc-url "''${CI_DEPLOY_RPC_URL}" --etherscan-api-key "''${EXPLORER_VERIFICATION_KEY}" \
               --sig='run(bytes)' \
               "$( ${(build-dispair-meta-cmd)} -E hex )" \
             ;
