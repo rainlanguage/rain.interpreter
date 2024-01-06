b021552866304abab8d7511d51aee8a32cdb477b
thedavidmeister <thedavidmeister@gmail.com>
2024-01-06 19:01:51 +0400

    wip on rainix

--- flake.nix
+++ flake.nix
@@ -2,40 +2,31 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
-    rain.url = "github:rainprotocol/rain.cli/6a912680be6d967fd6114aafab793ebe8503d27b";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    foundry.url = "github:shazow/foundry.nix/monthly";
+    rainix.url = "github:rainprotocol/rainix/915ad139d76afd9aab098cc88191be20c1bbfd07";
   };
 
-  outputs = { self, nixpkgs, rain, flake-utils, rust-overlay, foundry, ... }:
+  outputs = { self, flake-utils, rainix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # pkgs = nixpkgs.legacyPackages.${system};
-        overlays =[ (import rust-overlay) foundry.overlay ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rain-cli-bin = "${rain.defaultPackage.${system}}/bin/rain";
-        forge-bin = "${foundry.defaultPackage.${system}}/bin/forge";
-
-      in with pkgs; rec {
+        rain-cli-bin = "${rainix.rain.defaultPackage.${system}}/bin/rain";
+        forge-bin = "${rainix.foundry.defaultPackage.${system}}/bin/forge";
+        pkgs = rainix.pkgs.${system};
+      in {
         packages = rec {
-
           build-dispair-meta-cmd = ''
             ${rain-cli-bin} meta build \
               -i <(${rain-cli-bin} meta solc artifact -c abi -i out/RainterpreterExpressionDeployerNPE2.sol/RainterpreterExpressionDeployerNPE2.json) -m solidity-abi-v2 -t json -e deflate -l en \
               -i <(${forge-bin} script --silent ./script/GetAuthoringMeta.sol && cat ./meta/AuthoringMeta.rain.meta) -m authoring-meta-v1 -t cbor -e deflate -l none \
           '';
 
-          build-meta = pkgs.writeShellScriptBin "build-meta" ''
+          build-meta = rainix.pkgs.writeShellScriptBin "build-meta" ''
             mkdir -p meta;
             ${forge-bin} build --force;
             ${(build-dispair-meta-cmd)} -o meta/RainterpreterExpressionDeployerNPE2.rain.meta;
           '';
 
-          deploy-dispair = pkgs.writeShellScriptBin "deploy-dispair" (''
+          deploy-dispair = rainix.pkgs.writeShellScriptBin "deploy-dispair" (''
             set -euo pipefail;
             mkdir -p meta;
             ${forge-bin} build --force;
@@ -51,9 +42,9 @@
           # For `nix develop`:
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            rust-bin.stable."1.75.0".default
-            foundry-bin
-            slither-analyzer
+            pkgs.rust-bin.stable."1.75.0".default
+            pkgs.foundry-bin
+            pkgs.slither-analyzer
           ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           ]);
