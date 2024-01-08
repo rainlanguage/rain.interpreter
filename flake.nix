b9084a0a6213127a318b0404e070c1a4e5333ecd
thedavidmeister <thedavidmeister@gmail.com>
2024-01-08 14:51:24 +0400

    fix ci

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         mkTaskLocal = name: rainix.mkTask.${system} { name = name; body = (builtins.readFile ./task/${name}.sh); };
       in {
         packages = rec {
-          build-meta = mkTaskLocal "build-meta";
+          build-dispair-meta = mkTaskLocal "build-dispair-meta";
         } // rainix.packages.${system};
 
         devShells = rainix.devShells.${system};
