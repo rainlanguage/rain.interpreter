61af682e269ae313781ffcd920a616704b6cee7d
thedavidmeister <thedavidmeister@gmail.com>
2024-01-08 12:51:00 +0400

    mktask from rainix

--- flake.nix
+++ flake.nix
@@ -3,45 +3,42 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    rainix.url = "github:rainprotocol/rainix/9e945f33349606c12f71559f947473766ed3f922";
-    rain.url = "github:rainprotocol/rain.cli/6a912680be6d967fd6114aafab793ebe8503d27b";
+    rainix.url = "github:rainprotocol/rainix/511d871b1d1c3e8bf8084dbdfb54b00add52d1e1";
   };
 
-  outputs = { self, flake-utils, rainix, rain, ... }:
+  outputs = { self, flake-utils, rainix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = rainix.pkgs.${system};
-        forge-bin = "${pkgs.foundry-bin}/bin/forge";
-        rain-bin = "${rain.defaultPackage.${system}}/bin/rain";
       in {
         packages = rec {
           build-dispair-meta-cmd = ''
-            ${rain-bin} meta build \
-              -i <(${forge-bin} script --silent ./script/GetAuthoringMeta.sol && cat ./meta/AuthoringMeta.rain.meta) -m authoring-meta-v1 -t cbor -e deflate -l none \
+            rain meta build \
+              -i <(forge script --silent ./script/GetAuthoringMeta.sol && cat ./meta/AuthoringMeta.rain.meta) -m authoring-meta-v1 -t cbor -e deflate -l none \
           '';
 
-          build-meta = pkgs.writeShellScriptBin "build-meta" ''
+          build-meta = rainix.mkTask.${system} { name = "build-meta"; body = ''
             mkdir -p meta;
-            ${forge-bin} build --force;
+            forge build --force;
             ${(build-dispair-meta-cmd)} -o meta/RainterpreterExpressionDeployerNPE2.rain.meta;
-          '';
+          ''; };
 
-          deploy-dispair = pkgs.writeShellScriptBin "deploy-dispair" (''
+          deploy-dispair = rainix.mkTask.${system} {name = "deploy-dispair"; body = (''
             set -euo pipefail;
             mkdir -p meta;
-            ${forge-bin} build --force;
-            ${forge-bin} script -vvvvv script/DeployDISPair.sol --legacy --verify --broadcast --rpc-url "''${CI_DEPLOY_RPC_URL}" --etherscan-api-key "''${EXPLORER_VERIFICATION_KEY}" \
+            forge build --force;
+            forge script -vvvvv script/DeployDISPair.sol --legacy --verify --broadcast --rpc-url "''${CI_DEPLOY_RPC_URL}" --etherscan-api-key "''${EXPLORER_VERIFICATION_KEY}" \
               --sig='run(bytes)' \
               "$( ${(build-dispair-meta-cmd)} -E hex )" \
             ;
-          '');
+          ''); };
 
-          ci-prep = pkgs.writeShellScriptBin "ci-prep" ''
+          ci-prep = rainix.mkTask.${system} { name = "ci-prep"; body = ''
             mkdir -p meta;
-            ${forge-bin} install --shallow;
-            ${forge-bin} build --force;
+            forge install --shallow;
+            forge build --force;
             ${(build-dispair-meta-cmd)} -o meta/RainterpreterExpressionDeployerNPE2.rain.meta;
-          '';
+          ''; };
 
           default = build-meta;
         } // rainix.packages.${system};
