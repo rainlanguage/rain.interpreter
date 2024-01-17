5648e8417c7d94171ced67422158bc51b8b0de2b
Josh <jks.hardy@gmail.com>
2024-01-17 17:51:14 +0000

    wip

--- crates/dispair/src/lib.rs
+++ crates/dispair/src/lib.rs
@@ -1,22 +1,18 @@
-use crate::registry::IExpressionDeployerV3;
-use crate::registry::{IInterpreterStoreV1, IInterpreterV2, IParserV1};
-use ethers::contract::ContractError;
-use ethers::{
-    providers::{Http, Provider},
-    types::H160,
+use rain_interpreter_bindings::{
+    IExpressionDeployerV3, IInterpreterStoreV1, IInterpreterV2, IParserV1,
 };
 
 /// DISPair
 /// Struct representing DISP instances.
 pub struct DISPair {
-    pub deployer: IExpressionDeployerV3<Provider<Http>>,
+    pub deployer: IExpressionDeployerV3,
     pub interpreter: IInterpreterV2<Provider<Http>>,
     pub store: IInterpreterStoreV1<Provider<Http>>,
     pub parser: IParserV1<Provider<Http>>,
 }
 
 /// Implementation to build DISPair from Deployer instance.
-impl IExpressionDeployerV3<Provider<Http>> {
+impl IExpressionDeployerV {
     pub async fn try_build_dispair(&self) -> Result<DISPair, ContractError<Provider<Http>>> {
         Ok(DISPair {
             deployer: self.clone(),
