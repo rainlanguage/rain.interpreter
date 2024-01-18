b491d77aecff9216ba77c2c8ff06b45231685170
Josh <jks.hardy@gmail.com>
2024-01-18 12:25:14 +0000

    tests

--- crates/dispair/src/lib.rs
+++ crates/dispair/src/lib.rs
@@ -1,6 +1,7 @@
-use alloy_ethers_typecast::transaction::{ReadContractParametersBuilder, ReadableClientHttp};
+use alloy_ethers_typecast::transaction::{ReadContractParametersBuilder, ReadableClient};
 use alloy_primitives::*;
 use anyhow::*;
+use ethers::providers::JsonRpcClient;
 use rain_interpreter_bindings::DeployerISP;
 
 /// DISPair
@@ -15,9 +16,9 @@ pub struct DISPair {
 
 /// Implementation to build DISPair from Deployer address.
 impl DISPair {
-    pub async fn from_deployer(
+    pub async fn from_deployer<T: JsonRpcClient>(
         deployer: Address,
-        client: ReadableClientHttp,
+        client: ReadableClient<T>,
     ) -> anyhow::Result<Self> {
         Ok(DISPair {
             deployer,
@@ -49,5 +50,62 @@ impl DISPair {
                 .await?
                 ._0,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::Address;
+    use ethers::providers::{MockProvider, MockResponse, Provider};
+    use serde_json::json;
+    use tracing_subscriber::FmtSubscriber;
+
+    #[tokio::test]
+    async fn test_from_deployer() -> Result<(), Error> {
+        setup_tracing();
+
+        // MockProvider for testing
+        let transport = MockProvider::default();
+        let deployer_address = "0x1234567890123456789012345678901234567890".parse::<Address>()?;
+        let interpreter_address = "1234567890123456789012345678901234567891";
+        let store_address = "1234567890123456789012345678901234567892";
+        let parser_address = "1234567890123456789012345678901234567893";
+
+        // Mock responses for the read calls - the responses will be popped off
+        // the stack in the reverse order they are pushed on.
+        transport.push_response(MockResponse::Value(json!(format!(
+            "0x{:0>64}",
+            parser_address
+        ))));
+
+        transport.push_response(MockResponse::Value(json!(format!(
+            "0x{:0>64}",
+            store_address
+        ))));
+
+        transport.push_response(MockResponse::Value(json!(format!(
+            "0x{:0>64}",
+            interpreter_address
+        ))));
+
+        let client = ReadableClient::new(Provider::new(transport));
+        let dispair = DISPair::from_deployer(deployer_address, client).await?;
+
+        assert_eq!(dispair.deployer, deployer_address);
+        assert_eq!(dispair.interpreter, interpreter_address.parse::<Address>()?);
+        assert_eq!(dispair.store, store_address.parse::<Address>()?);
+        assert_eq!(dispair.parser, parser_address.parse::<Address>()?);
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    fn setup_tracing() {
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(tracing::Level::DEBUG)
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("Failed to set tracing subscriber");
     }
 }
