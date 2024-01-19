5370de009f34f53642a4dacacf287d833ab8f9f6
thedavidmeister <thedavidmeister@gmail.com>
2024-01-19 20:15:53 +0400

    prb pow

--- src/lib/op/math/decimal18/LibOpDecimal18PowNP.sol
+++ src/lib/op/math/decimal18/LibOpDecimal18PowNP.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
-import {UD60x18, powu} from "prb-math/UD60x18.sol";
+import {UD60x18, pow} from "prb-math/UD60x18.sol";
 import {Operand} from "../../../../interface/unstable/IInterpreterV2.sol";
 import {Pointer} from "rain.solmem/lib/LibPointer.sol";
 import {InterpreterStateNP} from "../../../state/LibInterpreterStateNP.sol";
 import {IntegrityCheckStateNP} from "../../../integrity/LibIntegrityCheckNP.sol";
 
-/// @title LibOpDecimal18PowUNP
-/// @notice Opcode to pow N 18 decimal fixed point values to a uint256 power.
+/// @title LibOpDecimal18PowNP
+/// @notice Opcode to pow N 18 decimal fixed point values to an 18 decimal power.
 library LibOpDecimal18PowUNP {
     function integrity(IntegrityCheckStateNP memory, Operand) internal pure returns (uint256, uint256) {
         // There must be two inputs and one output.
         return (2, 1);
     }
 
-    /// decimal18-pow-u
+    /// decimal18-pow
     /// 18 decimal fixed point exponentiation with implied overflow checks from
     /// PRB Math.
     function run(InterpreterStateNP memory, Operand, Pointer stackTop) internal pure returns (Pointer) {
@@ -26,7 +26,7 @@ library LibOpDecimal18PowUNP {
             stackTop := add(stackTop, 0x20)
             b := mload(stackTop)
         }
-        a = UD60x18.unwrap(powu(UD60x18.wrap(a), b));
+        a = UD60x18.unwrap(pow(UD60x18.wrap(a), b));
 
         assembly ("memory-safe") {
             mstore(stackTop, a)
@@ -41,7 +41,7 @@ library LibOpDecimal18PowUNP {
         returns (uint256[] memory)
     {
         uint256[] memory outputs = new uint256[](1);
-        outputs[0] = UD60x18.unwrap(powu(UD60x18.wrap(inputs[0]), inputs[1]));
+        outputs[0] = UD60x18.unwrap(pow(UD60x18.wrap(inputs[0]), inputs[1]));
         return outputs;
     }
 }
