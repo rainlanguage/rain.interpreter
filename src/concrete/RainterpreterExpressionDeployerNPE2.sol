384344d0cf4be1ed6c1d04c0105034aecafac521
rouzwelt <rouzweltt@gmail.com>
2024-05-14 23:47:46 +0000

    init

--- src/concrete/RainterpreterExpressionDeployerNPE2.sol
+++ src/concrete/RainterpreterExpressionDeployerNPE2.sol
@@ -126,7 +126,10 @@ contract RainterpreterExpressionDeployerNPE2 is
 
     /// @inheritdoc IERC165
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IExpressionDeployerV4).interfaceId || interfaceId == type(IERC165).interfaceId;
+        return interfaceId == 
+            type(IExpressionDeployerV4).interfaceId
+            || interfaceId == type(IERC165).interfaceId
+            || interfaceId == type(IDescribedByMetaV1).interfaceId;
     }
 
     /// @inheritdoc IExpressionDeployerV4
