8f818842c0eb0fb6d688f261b5f47e6071edb562
thedavidmeister <thedavidmeister@gmail.com>
2024-06-09 22:20:59 +0400

    rm codegen

--- src/interface/IIntegrityToolingV1.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: CAL
-pragma solidity ^0.8.25;
-
-interface IIntegrityToolingV1 {
-    function buildIntegrityFunctionPointers() external view returns (bytes memory);
-}
--- src/interface/IOpcodeToolingV1.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: CAL
-pragma solidity ^0.8.25;
-
-interface IOpcodeToolingV1 {
-    function buildOpcodeFunctionPointers() external view returns (bytes memory);
-}
