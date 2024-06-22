4ee92993b514fe5c39743352e905ba81575617fe
thedavidmeister <thedavidmeister@gmail.com>
2024-06-23 00:46:44 +0400

    iparser v3

--- test/src/abstract/BaseRainterpreterSubParserNPE2.ierc165.t.sol
+++ test/src/abstract/BaseRainterpreterSubParserNPE2.ierc165.t.sol
@@ -3,7 +3,7 @@ pragma solidity =0.8.25;
 
 import {Test} from "forge-std/Test.sol";
 import {IERC165} from "openzeppelin-contracts/contracts/utils/introspection/IERC165.sol";
-import {ISubParserV2} from "rain.interpreter.interface/interface/ISubParserV2.sol";
+import {ISubParserV3} from "rain.interpreter.interface/interface/unstable/ISubParserV3.sol";
 import {BaseRainterpreterSubParserNPE2} from "src/abstract/BaseRainterpreterSubParserNPE2.sol";
 
 /// @dev We need a contract that is deployable in order to test the abstract
@@ -36,11 +36,11 @@ contract BaseRainterpreterSubParserNPE2IERC165Test is Test {
         bytes4 badInterfaceId = bytes4(badInterfaceIdUint);
 
         vm.assume(badInterfaceId != type(IERC165).interfaceId);
-        vm.assume(badInterfaceId != type(ISubParserV2).interfaceId);
+        vm.assume(badInterfaceId != type(ISubParserV3).interfaceId);
 
         ChildRainterpreterSubParserNPE2 subParser = new ChildRainterpreterSubParserNPE2();
         assertTrue(subParser.supportsInterface(type(IERC165).interfaceId));
-        assertTrue(subParser.supportsInterface(type(ISubParserV2).interfaceId));
+        assertTrue(subParser.supportsInterface(type(ISubParserV3).interfaceId));
         assertFalse(subParser.supportsInterface(badInterfaceId));
     }
 }
