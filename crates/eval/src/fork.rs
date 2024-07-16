3b471aaffa54026ed35014f5b4b9ce61ebd6f5ca
thedavidmeister <thedavidmeister@gmail.com>
2024-07-16 23:57:17 +0400

    fix fork

--- crates/eval/src/fork.rs
+++ crates/eval/src/fork.rs
@@ -418,7 +418,7 @@ mod tests {
     const USDT_POLYGON: &str = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
     const USDT_BSC: &str = "0x55d398326f99059fF775485246999027B3197955";
     const POLYGON_FORK_NUMBER: u64 = 54697866;
-    const BSC_FORK_NUMBER: u64 = 36281780;
+    const BSC_FORK_NUMBER: u64 = 40531873;
     const POLYGON_FORK_URL: &str = "https://rpc.ankr.com/polygon";
     const BSC_FORK_URL: &str = "https://rpc.ankr.com/bsc";
     const BSC_ACC: &str = "0xee5B5B923fFcE93A870B3104b7CA09c3db80047A";
