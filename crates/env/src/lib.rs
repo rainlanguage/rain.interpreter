f4d6b6244573fd2c82a87b8fe0f73d0961b7679b
thedavidmeister <thedavidmeister@gmail.com>
2024-07-16 22:51:26 +0400

    compile time env in test only

--- crates/env/src/lib.rs
+++ crates/env/src/lib.rs
@@ -1,10 +1,13 @@
 use alloy_primitives::{Address, BlockNumber};
 use once_cell::sync::Lazy;
 
+#[cfg(test)]
 pub const CI_DEPLOY_SEPOLIA_RPC_URL: &str = env!(
     "CI_DEPLOY_SEPOLIA_RPC_URL",
     "$CI_DEPLOY_SEPOLIA_RPC_URL not set."
 );
+
+#[cfg(test)]
 pub static CI_FORK_SEPOLIA_DEPLOYER_ADDRESS: Lazy<Address> = Lazy::new(|| {
     env!(
         "CI_FORK_SEPOLIA_DEPLOYER_ADDRESS",
@@ -13,6 +16,8 @@ pub static CI_FORK_SEPOLIA_DEPLOYER_ADDRESS: Lazy<Address> = Lazy::new(|| {
     .parse()
     .unwrap()
 });
+
+#[cfg(test)]
 pub static CI_FORK_SEPOLIA_BLOCK_NUMBER: Lazy<BlockNumber> = Lazy::new(|| {
     env!(
         "CI_FORK_SEPOLIA_BLOCK_NUMBER",
