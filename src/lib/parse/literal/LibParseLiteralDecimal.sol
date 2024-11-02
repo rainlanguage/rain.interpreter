e27510a64e293e2943d444d8aee577361a87e4c0
thedavidmeister <thedavidmeister@gmail.com>
2024-11-02 17:46:48 +0400

    wip

--- src/lib/parse/literal/LibParseLiteralDecimal.sol
+++ src/lib/parse/literal/LibParseLiteralDecimal.sol
@@ -1,37 +1,22 @@
-// SPDX-License-Identifier: CAL
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
 pragma solidity ^0.8.18;
 
 import {ParseState} from "../LibParseState.sol";
-import {
-    DecimalLiteralOverflow,
-    ZeroLengthDecimal,
-    MalformedExponentDigits,
-    MalformedDecimalPoint,
-    DecimalLiteralPrecisionLoss
-} from "../../../error/ErrParse.sol";
-import {
-    CMASK_E_NOTATION,
-    CMASK_NUMERIC_0_9,
-    CMASK_DECIMAL_POINT,
-    CMASK_NEGATIVE_SIGN,
-    CMASK_ZERO
-} from "rain.string/lib/parse/LibParseCMask.sol";
-import {LibParseChar} from "rain.string/lib/parse/LibParseChar.sol";
-import {LibParseDecimal} from "rain.string/lib/parse/LibParseDecimal.sol";
 import {LibParseError} from "../LibParseError.sol";
-import {LibParse} from "../LibParse.sol";
-import {LibDecimalFloatImplementation, LibDecimalFloat} from "rain.math.float/lib/LibDecimalFloat.sol";
+import {LibParseDecimalFloat} from "rain.math.float/lib/parse/LibParseDecimalFloat.sol";
 
 library LibParseLiteralDecimal {
     using LibParseError for ParseState;
-    using LibParseLiteralDecimal for ParseState;
 
-    function parseDecimalFloatPacked(ParseState memory state) internal view returns (LibDecimalFloat.Data memory) {
-        LibDecimalFloat.Data memory result;
-        result = self.parseDecimalFloat();
-        self.assertEnd();
-        return result;
+    function parseDecimalFloatPacked(ParseState memory state, uint256 start, uint256 end)
+        internal
+        pure
+        returns (uint256, uint256)
+    {
+        (bytes4 errorSelector, uint256 cursor, uint256 packedFloat) =
+            LibParseDecimalFloat.parseDecimalFloatPacked(start, end);
+        state.handleErrorSelector(errorSelector, cursor);
+        return (cursor, packedFloat);
     }
-
-
 }
