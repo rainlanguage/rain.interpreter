e2fdf3b644c2de2e0fa03638d58d89498188cc83
David Meister <thedavidmeister@gmail.com>
2025-07-18 16:05:05 +0400

    Update src/lib/op/math/uint256/LibOpUint256Sub.sol
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/lib/op/math/uint256/LibOpUint256Sub.sol
+++ src/lib/op/math/uint256/LibOpUint256Sub.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {IntegrityCheckState} from "../../../integrity/LibIntegrityCheck.sol";
 import {InterpreterState} from "../../../state/LibInterpreterState.sol";
