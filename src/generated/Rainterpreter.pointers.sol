bf9449322d9a1bb97db5afb13faa3ea38c54c68b
thedavidmeister <thedavidmeister@gmail.com>
2025-07-22 21:07:17 +0400

    ensure float zero

--- src/generated/Rainterpreter.pointers.sol
+++ src/generated/Rainterpreter.pointers.sol
@@ -10,11 +10,11 @@
 pragma solidity =0.8.25;
 
 /// @dev Hash of the known bytecode.
-bytes32 constant BYTECODE_HASH = bytes32(0x7770f5e2359b2a92d0b5cb50af2f0c9dd90e78a0e0121c6ae2e6ac1da0cde34e);
+bytes32 constant BYTECODE_HASH = bytes32(0x557ce3005477057bff2ef47b45953f4069c69846cc1287e41fa6837f3ffdd616);
 
 /// @dev The function pointers known to the interpreter for dynamic dispatch.
 /// By setting these as a constant they can be inlined into the interpreter
 /// and loaded at eval time for very low gas (~100) due to the compiler
 /// optimising it to a single `codecopy` to build the in memory bytes array.
 bytes constant OPCODE_FUNCTION_POINTERS =
-    hex"07e10813083709c30a8c0a9e0ab00ac90aed0b210b320b430be50c040cc20d720df60f38106b0cc21164121612b81330134113521352136313aa142c144514591472148b14b614cf14e8150f1522158415d21620166e16bc170a17581789179717e518161864189518e319311a18";
+    hex"07e10813083709c30a8c0a9e0ab00ac90aed0b210b320b430be50c040cc20d720df60f38106b0cc21164121612b81330134113521352136313aa1455146e1482149b14b414df14f815111538154b15ad15fb1649169716e51733178117b217c0180e183f188d18be190c195a1a41";
